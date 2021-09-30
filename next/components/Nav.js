4dbaa3376c015afcfd5e412246d73137fd36ff30
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-09-30 20:06:26 +0530

    stylings updated css

---
+++ next/components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <nav>
+      <h2>Logo</h2>
+
+      <ul>
+        <li>Link 1</li>
+        <li>Link 2</li>
+      </ul>
+    </nav>
+  );
+}
