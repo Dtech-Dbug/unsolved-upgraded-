69f8ebf5acab06d8f9281d3b5c053615c20d899f
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-09-30 22:37:13 +0530

    added fontawesome

--- next/pages/index.js
+++ next/pages/index.js
@@ -1,13 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
 import Nav from "../components/Nav";
+import Intro from "../components/Intro";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <Nav />
-      <h2>First </h2>
+      <Nav navLinkOne="Blog" navLinkTwo="Login" />
+      <Intro />
     </div>
   );
 }
